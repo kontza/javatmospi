2ba182b5c50ee2688d59b5e61ac40085e34b383b
Rumpu-Jussi <kontza@gmail.com>
2017-01-08 13:48:52 +0200

    Added latest temperature controller and a test for it.

--- src/main/java/org/kontza/atmospi/AtmospiLatestTemperatureController.java
+++ src/main/java/org/kontza/atmospi/AtmospiLatestTemperatureController.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.kontza.atmospi;
+
+import java.util.ArrayList;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author juharuotsalainen
  */
+@RestController
 public class AtmospiLatestTemperatureController {
 
+    private Logger logger = LoggerFactory.getLogger(AtmospiSettingsController.class.getName());
+
+    @Autowired
+    private AtmospiSettings settings;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Autowired
+    private TemperatureRepository temperatureRepository;
+
+    @RequestMapping("/data/latest/temperature")
+    public String settings() {
+        JSONObject retVal = new JSONObject();
+        Iterable<Device> devices = deviceRepository.findAll();
+        for (Device d : devices) {
+            logger.info("Device: {}", d.getLabel());
+            Temperature temperature = temperatureRepository.findTop1ByDeviceIdOrderByTimestampDesc(d.getDeviceid());
+            ArrayList<String> values = new ArrayList<>();
+            values.add(String.format("%d", temperature.getTimestamp()));
+            if (settings.getTemperatureUnit().equals(AtmospiSettings.TEMP_C)) {
+                values.add(String.format("%f", temperature.getC()));
+            } else {
+                values.add(String.format("%f", temperature.getF()));
+            }
+            retVal.put(d.getLabel(), values);
+        }
+        return retVal.toString();
+    }
 }
--- src/test/java/org/kontza/atmospi/AtmospiApplicationTests.java
+++ src/test/java/org/kontza/atmospi/AtmospiApplicationTests.java
@@ -1,7 +1,11 @@
 package org.kontza.atmospi;
 
+import java.util.Set;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -21,6 +25,11 @@ public class AtmospiApplicationTests {
 
     private final Logger logger = LoggerFactory.getLogger(AtmospiApplicationTests.class.getName());
 
+    // These values are from the log.sqlite -file in this repo.
+    private final String latestDeviceLabel = "28-000003ea01f5";
+    private final int latestTimestamp = 1477576501;
+    private final double latestTemperature = 8.94;
+
     @Autowired
     private AtmospiSettings settings;
 
@@ -38,5 +47,21 @@ public class AtmospiApplicationTests {
         entity = restTemplate.getForEntity("/settings", Settings.class);
         logger.info("Entity status code: {}", entity.getStatusCodeValue());
         assertEquals(entity.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void getLatestTemperature() {
+        final ResponseEntity<String> entity;
+        entity = restTemplate.getForEntity("/data/latest/temperature", String.class);
+        logger.info("Latest temp: {}", entity.getBody());
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        JSONObject o = new JSONObject(entity.getBody());
+        Set<String> keys = o.keySet();
+        assertTrue(keys.contains(latestDeviceLabel));
+        JSONArray array = o.optJSONArray(latestDeviceLabel);
+        // The first item is the timestamp.
+        assertEquals(array.getLong(0), latestTimestamp);
+        // The second item is the temperature in centigrade.
+        assertEquals(array.getDouble(1), latestTemperature, 0);
     }
 }
