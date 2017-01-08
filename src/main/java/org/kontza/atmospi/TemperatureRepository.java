c80d69b2afb93f922422fd80fd475c0dda8722c1
Rumpu-Jussi <kontza@gmail.com>
2017-01-08 15:20:54 +0200

    New find-methods added.

--- src/main/java/org/kontza/atmospi/TemperatureRepository.java
+++ src/main/java/org/kontza/atmospi/TemperatureRepository.java
@@ -1,11 +1,14 @@
 package org.kontza.atmospi;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TemperatureRepository extends CrudRepository<Temperature, Long> {
 
-    public Iterable<Temperature> findAllByDeviceId(Long deviceId);
+    public List<Temperature> findAllByDeviceId(Long deviceId);
 
     public Temperature findTop1ByDeviceIdOrderByTimestampDesc(Long deviceid);
+
+    public List<Temperature> findByDeviceIdAndTimestampBetween(Long deviceId, Long rangeMin, Long rangeMax);
 
 }
