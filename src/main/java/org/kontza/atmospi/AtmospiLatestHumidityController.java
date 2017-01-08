5c3399c10eaba870c446e011eb78e624ca103f1b
Rumpu-Jussi <kontza@gmail.com>
2017-01-08 19:03:45 +0200

    Foo controller added.

--- src/main/java/org/kontza/atmospi/AtmospiLatestHumidityController.java
+++ src/main/java/org/kontza/atmospi/AtmospiLatestHumidityController.java
@@ -1,5 +1,18 @@
 package org.kontza.atmospi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AtmospiLatestHumidityController {
 
+    private Logger logger = LoggerFactory.getLogger(AtmospiLatestHumidityController.class.getName());
+
+    @RequestMapping("/data/latest/humidity")
+    public String latest() throws Exception {
+        logger.info("Returning an empty object for latest humidity.");
+        return "{}";
+    }
 }
