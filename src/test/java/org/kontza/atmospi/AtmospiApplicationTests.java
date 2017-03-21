f3b6d255f9468bf9c3a93631187e0fe92d06f355
Rumpu-Jussi <kontza@gmail.com>
2017-03-21 23:07:30 +0200

    Tests updated to go together with the generated data set.

--- src/test/java/org/kontza/atmospi/AtmospiApplicationTests.java
+++ src/test/java/org/kontza/atmospi/AtmospiApplicationTests.java
@@ -28,16 +28,8 @@ public class AtmospiApplicationTests {
     private final Logger logger = LoggerFactory.getLogger(AtmospiApplicationTests.class.getName());
 
     // These values are from the log.sqlite -file in this repo.
-    private final String latestDeviceLabel = "Kasvihuone";
-    private final long latestTimestamp = 1477576501L;
-    private final double latestTemperature = 8.94;
-    private final double alphaTemperature = 9.06;
-    private final double omegaTemperature = 9.0;
-    private final String onlyDeviceId = "1";
-    private final long alpha = 1477563601L;
-    private final long omega = 1477570502L;
-    private final int rangeCount = 24;
-    private final int totalCount = 518;
+    private final String onlyDeviceLabel = "Kasvihuone";
+    private final long onlyDeviceId = 1;
     private final HashMap<String, Long> parms = new HashMap<>();
 
     @Autowired
@@ -45,6 +37,9 @@ public class AtmospiApplicationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TemperatureRepository temperatureRepository;
 
     @Test
     public void settingsLoaded() {
@@ -61,22 +56,23 @@ public class AtmospiApplicationTests {
 
     @Test
     public void getLatestTemperature() {
+        Temperature t = temperatureRepository.findTop1ByDeviceIdOrderByTimestampDesc(onlyDeviceId);
         final ResponseEntity<String> entity;
         entity = restTemplate.getForEntity("/data/latest/temperature", String.class);
         logger.info("Latest temp: {}", entity.getBody());
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         JSONObject o = new JSONObject(entity.getBody());
         Set<String> keys = o.keySet();
-        assertTrue(keys.contains(latestDeviceLabel));
+        assertTrue(keys.contains(onlyDeviceLabel));
         logger.info(">>> KEYS: {}", keys);
-        JSONArray array = o.optJSONArray(latestDeviceLabel);
+        JSONArray array = o.optJSONArray(onlyDeviceLabel);
         // The first item is the timestamp.
-        assertEquals(array.getLong(0), 1000 * latestTimestamp);
+        assertEquals(1000*t.getTimestamp(), array.getLong(0));
         // The second item is the temperature in centigrade.
         Double d = Double.parseDouble(array.getString(1));
         logger.info(">>> Converted: {}", d);
-        logger.info(">>> Expected : {}", latestTemperature);
-        assertEquals(d, latestTemperature, 0);
+        logger.info(">>> Expected : {}", t.getC());
+        assertEquals(t.getC(), d, 0.001);
     }
 
     @Test
@@ -87,33 +83,36 @@ public class AtmospiApplicationTests {
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         JSONObject o = new JSONObject(entity.getBody());
         Set<String> keys = o.keySet();
-        assertTrue(keys.contains(onlyDeviceId));
-        String label = o.getString(onlyDeviceId);
-        assertEquals(latestDeviceLabel, label);
+        String onlyDeviceIdAsString = String.format("%d", onlyDeviceId);
+        assertTrue(keys.contains(onlyDeviceIdAsString));
+        String label = o.getString(onlyDeviceIdAsString);
+        assertEquals(onlyDeviceLabel, label);
     }
 
     @Test
     public void getRangedTemperatures() {
-        parms.put("rangeMin", alpha);
-        parms.put("rangeMax", omega);
+        Temperature alphaTemp = temperatureRepository.findTop1ByDeviceIdOrderByTimestampAsc(onlyDeviceId);
+        Temperature omegaTemp = temperatureRepository.findTop1ByDeviceIdOrderByTimestampDesc(onlyDeviceId);
+        parms.put("rangeMin", alphaTemp.getTimestamp());
+        parms.put("rangeMax", omegaTemp.getTimestamp());
         String urlTemplate = "/data/device/1/temperature/?range_min={rangeMin}&range_max={rangeMax}";
         final ResponseEntity<String> entity;
         entity = restTemplate.exchange(urlTemplate, HttpMethod.GET, null, String.class, parms);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         JSONArray o = new JSONArray(entity.getBody());
         logger.info("Temperatures: {}", o.toString());
-        assertEquals(rangeCount, o.length());
+        assertEquals(temperatureRepository.countByDeviceId(onlyDeviceId), o.length());
         JSONArray array = o.getJSONArray(0);
         // The first item is the timestamp.
-        assertEquals(array.getLong(0), 1000 * alpha);
+        assertEquals(array.getLong(0), 1000 * alphaTemp.getTimestamp());
         // The second item is the temperature in centigrade.
-        assertEquals(alphaTemperature, array.getDouble(1), 0);
+        assertEquals(alphaTemp.getC(), array.getDouble(1), 0.001);
         // Check the last item.
         array = o.getJSONArray(o.length() - 1);
         // The first item is the timestamp.
-        assertEquals(array.getLong(0), 1000 * omega);
+        assertEquals(array.getLong(0), 1000 * omegaTemp.getTimestamp());
         // The second item is the temperature in centigrade.
-        assertEquals(omegaTemperature, array.getDouble(1), 0);
+        assertEquals(omegaTemp.getC(), array.getDouble(1), 0.001);
     }
 
     @Test
@@ -123,6 +122,6 @@ public class AtmospiApplicationTests {
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         JSONArray o = new JSONArray(entity.getBody());
         logger.info("Temperatures count: {}", o.length());
-        assertTrue(o.length() >= totalCount);
+        assertTrue(o.length() >= temperatureRepository.countByDeviceId(onlyDeviceId));
     }
 }
