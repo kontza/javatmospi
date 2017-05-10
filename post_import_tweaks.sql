3b4af8c401982568c5aa581311031a1591a730a6
kontza <kontza@gmail.com>
2017-05-10 08:13:59 +0300

    Quotes removed.

--- post_import_tweaks.sql
+++ post_import_tweaks.sql
@@ -10,11 +10,11 @@ alter table flag add column id serial primary key;
 -- Convert integer-columns into bigints.
 alter table devices alter deviceid type bigint;
 alter table flag alter deviceid type bigint;
-alter table flag alter 'timestamp' type bigint;
+alter table flag alter timestamp type bigint;
 alter table humidity alter deviceid type bigint;
-alter table humidity alter 'timestamp' type bigint;
+alter table humidity alter timestamp type bigint;
 alter table temperature alter deviceid type bigint;
-alter table temperature alter 'timestamp' type bigint;
+alter table temperature alter timestamp type bigint;
 
 -- Resync sequences.
 select setval('devices_deviceid_seq', (select max(deviceid) from devices));
